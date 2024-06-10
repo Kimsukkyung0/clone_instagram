c83809d3f9992891e9d1c39ef63cb23629331632
kimsukkyung0 <smk93021@gmail.com>
2024-06-10 22:13:13 +0900

    파일드래그앤드랍(3/6) 33ㅣ32

--- src/Components/Post/CreatePostModal.jsx
+++ src/Components/Post/CreatePostModal.jsx
@@ -1,41 +1,45 @@
-import React,{useState} from 'react'
-import { Modal, ModalOverlay, ModalContent, ModalBody,Button } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { Modal, ModalOverlay, ModalContent, ModalBody, Button } from '@chakra-ui/react'
 import { FaPhotoVideo } from 'react-icons/fa';
+import "./CreatePostModal.css"
 
 const CreatePostModal = (
     onClose, isOpen
 ) => {
-    const [isDragOver,setIsDragOver] = useState(false)
-    const [file,setFile]=useState();
-    const handleDrop=(event)=>{
+    const [isDragOver, setIsDragOver] = useState(false)
+    const [file, setFile] = useState();
+    const handleDrop = (event) => {
         event.preventDefault()
         const droppedFile = event.dataTransfer.file[0];
-        if(droppedFile.type.startsWith("image/") || droppedFile.type.startWith("video/")){
+        if (droppedFile.type.startsWith("image/") || droppedFile.type.startWith("video/")) {
             setFile(droppedFile)
         }
     }
-    
-    const handleDragOver=(event)=>{
+
+    const handleDragOver = (event) => {
         event.preventDefault();
-        event.dataTransfer.dropEffect="copy";
+        event.dataTransfer.dropEffect = "copy";
         setIsDragOver(true)
     }
-    const handleDragLeave=()=>{
+    const handleDragLeave = () => {
         setIsDragOver(false)
     }
 
-    const handleOnChange=(e)=>{
-        const file = e.target.file[0];
-        if(file && (file.type.starstWith("image/*")|| file.type.startsWith("video/"))){
+    const handleOnChange = (e) => {
+        
+        const file = e.target.files[0];
+        console.log("fiile : ",file);
+        if (file && (file.type.startsWith("image/") || file.type.startsWith("video/"))) {
             setFile(file);
-        }else{
+            console.log("fiile : ",file);
+        } else {
             setFile(null);
             alert("Please select an image or video")
         }
-
+    }
     return (
         <div>
-            <Modal className='w-[50%]' onClose={onClose} isOpen={isOpen} isCentered>
+            <Modal onClose={onClose} isOpen={isOpen} isCentered>
                 <ModalOverlay />
                 <ModalContent>
                     <div className='flex justify-between py-1 px-10 items-center'>
@@ -47,19 +51,22 @@ const CreatePostModal = (
                     <hr />
                     <ModalBody>
                         <div>
-                            <div>
-                                <div onDrop={handleDrop}
-                                onDragOver={handleDragOver}
-                                onDragLeave={handleDragLeave}
-                                className='drag-drop h-full'
-                                >
-                                    <div>
-                                        <FaPhotoVideo className='text-6xl'/>
-                                        <p>Drag photos and videos here</p>
+                            <div className='w-[50%]'>
+                                {!file &&
+                                    <div onDrop={handleDrop}
+                                        onDragOver={handleDragOver}
+                                        onDragLeave={handleDragLeave}
+                                        className='drag-drop h-full'
+                                    >
+                                        <div>
+                                            <FaPhotoVideo className='text-6xl' />
+                                            <p>Drag photos and videos here</p>
+                                        </div>
+                                        <label htmlFor="file-upload" className='custom-file-upload'>Select From Computer</label>
+                                        <input className="fileInput" type="file" id="file-upload" accept="image/*, video/*" onChange={handleOnChange} />
                                     </div>
-                                    <label htmlFor="file-upload" className='custom-file-upload'>Select From Computer</label>
-                                    <input type="text" id="file-upload" accept="image/*, video/*" onchange={handleOnChange} />
-                                </div>
+                                }
+                                {file && <img src={URL.createObjectURL(file)} alt="" />}
                             </div>
                         </div>
                     </ModalBody>
@@ -69,4 +76,4 @@ const CreatePostModal = (
     )
 }
 
-export default CreatePostModal
+export default CreatePostModal;
